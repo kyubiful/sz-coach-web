9e5064d9f18027f2d6839563d6cda4e30011f0e7
Kyubiful <ser.zabala@gmail.com>
2023-01-08 16:10:46 +0100

    feat: :sparkles: create blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,21 @@
-export default function BlogPage () {
+import { getPostsMetadata } from '../../services/blog.services'
+import Link from 'next/link'
+
+const PostCard = (post: any) => {
   return (
-    <h1>Página Blog</h1>
+    <div>
+      <p>{post.title}</p>
+      <p>{post.description}</p>
+      <Link href={`/blog/${post.slug}`}>{post.slug}</Link>
+    </div>
+  )
+}
+
+export default async function BlogPage () {
+  const postsMetadata = await getPostsMetadata()
+  return (
+    <div>
+      {postsMetadata && postsMetadata.map((post: any, key) => <PostCard key={key} {...post} />)}
+    </div>
   )
 }
