cfbf438bf059d4d2671c109df85ffd4792fead56
Kyubiful <ser.zabala@gmail.com>
2023-02-05 21:12:58 +0100

    feat: :sparkles: show and save data

--- app/blog/[slug]/edit/components/PostEditor.tsx
+++ app/blog/[slug]/edit/components/PostEditor.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { MouseEvent, useState } from 'react'
+import { ChangeEvent, MouseEvent, useState } from 'react'
 import Editor from '@monaco-editor/react'
 import { PostMetaData } from '../../../../../types'
 
@@ -12,17 +12,38 @@ type Props = {
 
 export function PostEditor ({ data, slug, content }: Props) {
   const [postContent, setPostContent] = useState(content)
+  const [postTitle, setPostTitle] = useState(data.title)
+  const [postDescription, setPostDescription] = useState(data.description)
+  const [postActive, setPostActive] = useState(data.active)
+  const [responseCode, setResponseCode] = useState(undefined)
 
   const handleClick = (e: MouseEvent<HTMLElement>) => {
     e.preventDefault()
 
     const content = postContent
-    const body = { postData: content, slug }
+    const body = { postData: content, slug, title: postTitle, description: postDescription, active: postActive }
 
-    fetch('/api/post', { method: 'POST', body: JSON.stringify(body) })
+    fetch('/api/posts', { method: 'POST', body: JSON.stringify(body) })
       .then(d => d.json())
-      .then(a => console.log(a))
-    console.log(body)
+      .then(a => {
+        setResponseCode(a.status)
+        setTimeout(() => {
+          setResponseCode(undefined)
+        }, 1000)
+      })
+      .catch(e => console.log(e))
+  }
+
+  const editTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setPostTitle(e.target.value)
+  }
+
+  const editDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    setPostDescription(e.target.value)
+  }
+
+  const editActive = () => {
+    setPostActive(!postActive)
   }
 
   return (
@@ -40,18 +61,23 @@ export function PostEditor ({ data, slug, content }: Props) {
       <div style={{ margin: '0px 0px 10px 10px', width: '30%' }}>
         <div style={{ display: 'flex', flexDirection: 'column', marginBottom: 10 }}>
           <label style={{ marginBottom: 5 }}>Titulo</label>
-          <input type='text' value={data.title} />
+          <input name='title' type='text' value={postTitle} onChange={(e: ChangeEvent<HTMLInputElement>) => editTitle(e)} />
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', marginBottom: 10 }}>
           <label style={{ marginBottom: 5 }}>Descripción</label>
-          <input type='text' value={data.description} />
+          <input name='description' type='text' value={postDescription} onChange={(e: ChangeEvent<HTMLInputElement>) => editDescription(e)} />
         </div>
         <div style={{ display: 'flex', alignContent: 'center', alignItems: 'center', marginBottom: 10 }}>
           <label>Activo</label>
-          <input type='checkbox' defaultChecked={data.active} />
+          <input name='active' type='checkbox' defaultChecked={postActive} onChange={editActive} />
         </div>
         <button onClick={handleClick}>Guardar</button>
       </div>
+      {
+      responseCode === 200
+        ? <div style={{ position: 'absolute', top: '50%', left: '50%', backgroundColor: 'white', padding: '20px 50px', borderRadius: 10, border: '1px solid black' }}>Guardado Correctamente</div>
+        : null
+      }
     </div>
 
   )
