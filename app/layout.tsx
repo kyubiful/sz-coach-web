1ba78a4ad7433fef9d913d29dfc6e0a401900cce
Kyubiful <ser.zabala@gmail.com>
2023-02-05 21:08:46 +0100

    feat: :sparkles: add navigation and tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Navigation } from './Navigation'
+import './globals.css'
 
 export default function RootLayout ({
   children
@@ -8,7 +10,10 @@ export default function RootLayout ({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <Navigation />
+        {children}
+      </body>
     </html>
   )
 }
