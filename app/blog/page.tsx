6ae28dce585b0a0a5405effc74fa7f80ece5bc92
Kyubiful <ser.zabala@gmail.com>
2023-02-05 21:11:52 +0100

    feat: :sparkles: add internal link to postcard

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,18 +3,19 @@ import Link from 'next/link'
 
 const PostCard = (post: any) => {
   return (
-    <div>
-      <p>{post.title}</p>
-      <p>{post.description}</p>
-      <Link href={`/blog/${post.slug}`}>{post.slug}</Link>
-    </div>
+    <Link href={`/blog/${post.slug}`} style={{ textDecoration: 'none' }}>
+      <div style={{ backgroundColor: 'white', border: '1px solid black', borderRadius: 20, padding: '20px 50px', width: '500px', marginBottom: 10 }}>
+        <p style={{ color: 'black', fontSize: '30px' }}>{post.title}</p>
+        <p style={{ color: 'black', fontSize: '16px' }}>{post.description}</p>
+      </div>
+    </Link>
   )
 }
 
 export default async function BlogPage () {
-  const postsMetadata = await getPostsMetadata()
+  const postsMetadata = getPostsMetadata()
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       {postsMetadata && postsMetadata.map((post: any, key) => <PostCard key={key} {...post} />)}
     </div>
   )
